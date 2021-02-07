f5ac91c2cffbedc58398471b779c8c585fc28c4e
Kevin Tran <kevin_tran@live.fr>
2021-02-07 23:07:05 +0100

    Add tests for solving initialization

--- src/solving/initialization.rs
+++ src/solving/initialization.rs
@@ -1,47 +1,135 @@
-use crate::settings::PuzzleSetting;
-use crate::settings::Rule;
-use crate::settings::TokenSet;
-use crate::solving::states::CellState;
-use crate::solving::states::State;
-use crate::solving::states::Tokenset;
+use crate::settings;
+use crate::solving::states;
 
-pub fn initialize(setting: &PuzzleSetting) -> State {
-    let mut result = State {
-        tokensets: setting
-            .tokensets
-            .iter()
-            .map(|tokenset| match tokenset {
-                TokenSet::Symbols { grid, candidates } => {
-                    let grid = &setting.grids[*grid];
-                    let candidates = (0..grid.columns)
-                        .map(|_| {
-                            (0..grid.rows)
-                                .map(|_| CellState::Candidates(candidates.clone()))
-                                .collect()
-                        })
-                        .collect();
-                    Tokenset::Symbols(candidates)
-                }
-            })
-            .collect(),
-    };
-    for rule in &setting.rules {
-        match rule {
-            Rule::Sudoku {
-                tokenset,
-                regions: _,
-                givens,
-            } => {
-                for given in givens {
-                    match &mut result.tokensets[*tokenset] {
-                        Tokenset::Symbols(candidates) => {
-                            candidates[given.cell.x as usize][given.cell.y as usize] =
-                                CellState::Set(given.symbol);
-                        }
-                    }
-                }
+pub fn initialize(
+    setting: &settings::PuzzleSetting,
+) -> Result<states::State, Box<dyn std::error::Error>> {
+    let tokensets = setting
+        .tokensets
+        .iter()
+        .map(|tokenset| match tokenset {
+            settings::TokenSet::Symbols { grid, candidates } => {
+                let grid = &setting.grids[*grid];
+                let candidates = (0..grid.columns)
+                    .map(|_| {
+                        (0..grid.rows)
+                            .map(|_| states::CellState::Candidates(candidates.clone()))
+                            .collect()
+                    })
+                    .collect();
+                states::Tokenset::Symbols(candidates)
             }
-        }
+        })
+        .collect();
+    let constraints = settings::list_constraints(setting)?;
+    let result = states::State {
+        tokensets,
+        constraints,
+    };
+    Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::initialize;
+    use crate::settings;
+    use crate::settings::presets;
+    use crate::settings::presets::sudoku;
+    use crate::shapes;
+    use crate::solving::states;
+
+    #[test]
+    fn test_nominal() -> Result<(), Box<dyn std::error::Error>> {
+        let mut setting = settings::PuzzleSetting::default();
+        let grid = setting.add_grid(4, 4);
+        let regions = shapes::Region::grid_boxes(2, 2, 2, 2).collect();
+        let givens = presets::givens(vec![
+            "5...".to_string(),
+            "..6.".to_string(),
+            ".1..".to_string(),
+            "....".to_string(),
+        ]);
+        sudoku::add_symbolset(&mut setting, grid, 4, regions, givens);
+
+        let result = initialize(&setting)?;
+        let tokensets = vec![states::Tokenset::Symbols(vec![
+            vec![
+                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+            ],
+            vec![
+                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+            ],
+            vec![
+                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+            ],
+            vec![
+                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+            ],
+        ])];
+        let repartition = [('1', 1), ('2', 1), ('3', 1), ('4', 1)]
+            .iter()
+            .cloned()
+            .collect::<std::collections::HashMap<char, usize>>();
+        let mut constraints = Vec::new();
+        constraints.extend(shapes::Region::grid_boxes(2, 2, 2, 2).map(|region| {
+            settings::Constraint::SymbolRepartition {
+                tokenset: 0,
+                repartition: repartition.clone(),
+                region,
+            }
+        }));
+        constraints.extend(shapes::Region::rows(4, 4).map(|region| {
+            settings::Constraint::SymbolRepartition {
+                tokenset: 0,
+                repartition: repartition.clone(),
+                region,
+            }
+        }));
+        constraints.extend(shapes::Region::columns(4, 4).map(|region| {
+            settings::Constraint::SymbolRepartition {
+                tokenset: 0,
+                repartition: repartition.clone(),
+                region,
+            }
+        }));
+        constraints.extend(
+            [
+                settings::Constraint::GivenSymbol {
+                    tokenset: 0,
+                    cell: shapes::Cell { x: 0, y: 0 },
+                    symbol: '5',
+                },
+                settings::Constraint::GivenSymbol {
+                    tokenset: 0,
+                    cell: shapes::Cell { x: 2, y: 1 },
+                    symbol: '6',
+                },
+                settings::Constraint::GivenSymbol {
+                    tokenset: 0,
+                    cell: shapes::Cell { x: 1, y: 2 },
+                    symbol: '1',
+                },
+            ]
+            .iter()
+            .cloned(),
+        );
+        let expected = states::State {
+            tokensets,
+            constraints,
+        };
+        assert_eq!(result, expected);
+        Ok(())
     }
-    result
 }
--- src/solving/states.rs
+++ src/solving/states.rs
@@ -1,15 +1,18 @@
-#[derive(serde::Deserialize, serde::Serialize)]
+use crate::settings;
+
+#[derive(Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
 pub enum CellState {
     Set(char),
     Candidates(Vec<char>),
 }
 
-#[derive(serde::Deserialize, serde::Serialize)]
+#[derive(Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
 pub enum Tokenset {
     Symbols(Vec<Vec<CellState>>),
 }
 
-#[derive(serde::Deserialize, serde::Serialize)]
+#[derive(Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
 pub struct State {
     pub tokensets: Vec<Tokenset>,
+    pub constraints: Vec<settings::Constraint>,
 }
