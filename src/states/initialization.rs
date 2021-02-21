a55915156cd0cf863b3430fcbca6fbf71c1c277e
Kevin Tran <kevin_tran@live.fr>
2021-02-21 18:32:19 +0100

    Allow initialization for playing

--- src/states/initialization.rs
+++ src/states/initialization.rs
@@ -1,8 +1,14 @@
 use crate::settings;
 use crate::states;
 
+pub enum Purpose {
+    Playing,
+    Solving,
+}
+
 pub fn initialize(
     setting: &settings::PuzzleSetting,
+    purpose: Purpose,
 ) -> Result<states::State, Box<dyn std::error::Error>> {
     let tokensets = setting
         .tokensets
@@ -10,14 +16,7 @@ pub fn initialize(
         .map(|tokenset| match tokenset {
             settings::TokenSet::Symbols { grid, candidates } => {
                 let grid = &setting.grids[*grid];
-                let candidates = (0..grid.columns)
-                    .map(|_| {
-                        (0..grid.rows)
-                            .map(|_| states::CellState::Candidates(candidates.clone()))
-                            .collect()
-                    })
-                    .collect();
-                states::Tokenset::Symbols(candidates)
+                initialize_symbolset(grid, candidates, &purpose)
             }
         })
         .collect();
@@ -25,9 +24,25 @@ pub fn initialize(
     Ok(result)
 }
 
+fn initialize_symbolset(
+    grid: &settings::Grid,
+    candidates: &[char],
+    purpose: &Purpose,
+) -> states::Tokenset {
+    let default = match purpose {
+        Purpose::Playing => states::CellState::Empty,
+        Purpose::Solving => states::CellState::Candidates(candidates.to_vec()),
+    };
+    let candidates = (0..grid.columns)
+        .map(|_| (0..grid.rows).map(|_| default.clone()).collect())
+        .collect();
+    states::Tokenset::Symbols(candidates)
+}
+
 #[cfg(test)]
 mod tests {
     use super::initialize;
+    use super::Purpose;
     use crate::settings;
     use crate::settings::presets;
     use crate::settings::presets::sudoku;
@@ -35,7 +50,7 @@ mod tests {
     use crate::states;
 
     #[test]
-    fn test_nominal() -> Result<(), Box<dyn std::error::Error>> {
+    fn test_nominal_solving() -> Result<(), Box<dyn std::error::Error>> {
         let mut setting = settings::PuzzleSetting::default();
         let grid = setting.add_grid(4, 4);
         let regions = shapes::Region::grid_boxes(2, 2, 2, 2).collect();
@@ -47,37 +62,79 @@ mod tests {
         ]);
         sudoku::add_symbolset(&mut setting, grid, 4, regions, givens);
 
-        let result = initialize(&setting)?;
+        let result = initialize(&setting, Purpose::Solving)?;
+        let candidates = states::CellState::Candidates(vec!['1', '2', '3', '4']);
         let tokensets = vec![states::Tokenset::Symbols(vec![
             vec![
-                states::CellState::Candidates(vec!['1', '2', '3', '4']),
-                states::CellState::Candidates(vec!['1', '2', '3', '4']),
-                states::CellState::Candidates(vec!['1', '2', '3', '4']),
-                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+                candidates.clone(),
+                candidates.clone(),
+                candidates.clone(),
+                candidates.clone(),
             ],
             vec![
-                states::CellState::Candidates(vec!['1', '2', '3', '4']),
-                states::CellState::Candidates(vec!['1', '2', '3', '4']),
-                states::CellState::Candidates(vec!['1', '2', '3', '4']),
-                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+                candidates.clone(),
+                candidates.clone(),
+                candidates.clone(),
+                candidates.clone(),
             ],
             vec![
-                states::CellState::Candidates(vec!['1', '2', '3', '4']),
-                states::CellState::Candidates(vec!['1', '2', '3', '4']),
-                states::CellState::Candidates(vec!['1', '2', '3', '4']),
-                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+                candidates.clone(),
+                candidates.clone(),
+                candidates.clone(),
+                candidates.clone(),
             ],
             vec![
-                states::CellState::Candidates(vec!['1', '2', '3', '4']),
-                states::CellState::Candidates(vec!['1', '2', '3', '4']),
-                states::CellState::Candidates(vec!['1', '2', '3', '4']),
-                states::CellState::Candidates(vec!['1', '2', '3', '4']),
+                candidates.clone(),
+                candidates.clone(),
+                candidates.clone(),
+                candidates.clone(),
             ],
         ])];
-        let repartition = [('1', 1), ('2', 1), ('3', 1), ('4', 1)]
-            .iter()
-            .cloned()
-            .collect::<std::collections::HashMap<char, usize>>();
+        let expected = states::State { tokensets };
+        assert_eq!(result, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn test_nominal_playing() -> Result<(), Box<dyn std::error::Error>> {
+        let mut setting = settings::PuzzleSetting::default();
+        let grid = setting.add_grid(4, 4);
+        let regions = shapes::Region::grid_boxes(2, 2, 2, 2).collect();
+        let givens = presets::givens(vec![
+            "5...".to_string(),
+            "..6.".to_string(),
+            ".1..".to_string(),
+            "....".to_string(),
+        ]);
+        sudoku::add_symbolset(&mut setting, grid, 4, regions, givens);
+
+        let result = initialize(&setting, Purpose::Playing)?;
+        let tokensets = vec![states::Tokenset::Symbols(vec![
+            vec![
+                states::CellState::Empty,
+                states::CellState::Empty,
+                states::CellState::Empty,
+                states::CellState::Empty,
+            ],
+            vec![
+                states::CellState::Empty,
+                states::CellState::Empty,
+                states::CellState::Empty,
+                states::CellState::Empty,
+            ],
+            vec![
+                states::CellState::Empty,
+                states::CellState::Empty,
+                states::CellState::Empty,
+                states::CellState::Empty,
+            ],
+            vec![
+                states::CellState::Empty,
+                states::CellState::Empty,
+                states::CellState::Empty,
+                states::CellState::Empty,
+            ],
+        ])];
         let expected = states::State { tokensets };
         assert_eq!(result, expected);
         Ok(())
