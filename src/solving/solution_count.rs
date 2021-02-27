2d1e39679e328f367c4ec96bb382146dfeeadca2
Kevin Tran <kevin_tran@live.fr>
2021-02-27 16:46:50 +0100

    Allow solution count on playing state

--- src/solving/solution_count.rs
+++ src/solving/solution_count.rs
@@ -6,12 +6,21 @@ enum Iterator {
     Symbolset {
         tokenset: usize,
         cell: shapes::Cell,
-        candidates: std::collections::HashSet<char>,
+        candidates: Vec<char>,
     },
     Unset,
 }
 
 pub fn count_solutions(
+    setting: &settings::PuzzleSetting,
+    state: &mut states::State,
+) -> Result<usize, Box<dyn std::error::Error>> {
+    let constraints = settings::list_constraints(&setting)?;
+    do_count_solutions(setting, state, &constraints)
+}
+
+fn do_count_solutions(
+    setting: &settings::PuzzleSetting,
     state: &mut states::State,
     constraints: &[settings::Constraint],
 ) -> Result<usize, Box<dyn std::error::Error>> {
@@ -21,45 +30,58 @@ pub fn count_solutions(
         states::Status::Invalid => return Ok(0),
         states::Status::Ongoing => (),
     }
-    let iterator = find_pivot(&state);
+    let iterator = find_pivot(&setting, &state);
     match iterator {
         Iterator::Symbolset {
             tokenset,
             cell,
             candidates,
         } => {
+            let previous = {
+                let tokenset = &mut state.tokensets[tokenset];
+                let states::Tokenset::Symbols(cells) = tokenset;
+                cells[cell.y as usize][cell.x as usize].clone()
+            };
             for candidate in &candidates {
                 let tokenset = &mut state.tokensets[tokenset];
                 let states::Tokenset::Symbols(cells) = tokenset;
                 cells[cell.y as usize][cell.x as usize] = states::CellState::Set(*candidate);
-                result += count_solutions(state, constraints)?;
+                result += do_count_solutions(setting, state, constraints)?;
             }
             let tokenset = &mut state.tokensets[tokenset];
             let states::Tokenset::Symbols(cells) = tokenset;
-            cells[cell.y as usize][cell.x as usize] = states::CellState::Candidates(candidates);
+            cells[cell.y as usize][cell.x as usize] = previous;
         }
         Iterator::Unset => (),
     }
     Ok(result)
 }
 
-fn find_pivot(state: &states::State) -> Iterator {
+fn find_pivot(setting: &settings::PuzzleSetting, state: &states::State) -> Iterator {
     for (index, tokenset) in state.tokensets.iter().enumerate() {
         match tokenset {
             states::Tokenset::Symbols(cells) => {
                 for row in 0..cells.len() {
                     for cell in 0..cells[row].len() {
-                        if let states::CellState::Candidates(candidates) = &cells[row][cell] {
-                            let cell = shapes::Cell {
-                                x: cell as isize,
-                                y: row as isize,
-                            };
-                            return Iterator::Symbolset {
-                                tokenset: index,
-                                cell,
-                                candidates: candidates.clone(),
-                            };
+                        if let states::CellState::Set(_) = &cells[row][cell] {
+                            continue;
                         }
+                        let cell = shapes::Cell {
+                            x: cell as isize,
+                            y: row as isize,
+                        };
+                        let setting_tokenset = &setting.tokensets[index];
+                        let candidates = match setting_tokenset {
+                            settings::TokenSet::Symbols {
+                                grid: _,
+                                candidates,
+                            } => candidates.clone(),
+                        };
+                        return Iterator::Symbolset {
+                            tokenset: index,
+                            cell,
+                            candidates,
+                        };
                     }
                 }
             }
@@ -84,14 +106,13 @@ mod tests {
             "...4".to_string(),
         ];
         let setting = sudoku::setting(2, &grid);
-        let constraints = settings::list_constraints(&setting)?;
         let mut state = states::State {
             tokensets: vec![states::symbolset(
                 &grid,
                 states::CellState::Candidates("1234".chars().collect()),
             )],
         };
-        let result = count_solutions(&mut state, &constraints)?;
+        let result = count_solutions(&setting, &mut state)?;
         assert_eq!(result, 1);
         Ok(())
     }
@@ -105,15 +126,11 @@ mod tests {
             "...4".to_string(),
         ];
         let setting = sudoku::setting(2, &grid);
-        let constraints = settings::list_constraints(&setting)?;
         let mut state = states::State {
-            tokensets: vec![states::symbolset(
-                &grid,
-                states::CellState::Candidates("1234".chars().collect()),
-            )],
+            tokensets: vec![states::symbolset(&grid, states::CellState::Empty)],
         };
         let starting_state = state.clone();
-        let result = count_solutions(&mut state, &constraints)?;
+        let result = count_solutions(&setting, &mut state)?;
         assert_eq!(result, 1);
         assert_eq!(state, starting_state);
         Ok(())
